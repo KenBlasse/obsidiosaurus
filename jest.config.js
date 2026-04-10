6fcb45a0bf274cca8a12634e286ba7430b72230c
Alex Seitz <aseitz1307@googlemail.com>
2026-04-10 20:08:56 +0200

    chore: fix ts-jest deprecation, use transform instead of globals

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        module: 'commonjs',
+        esModuleInterop: true,
+      },
+    }],
+  },
   moduleNameMapper: {
     '^obsidian$': '<rootDir>/__mocks__/obsidian.ts',
     '^src/(.*)$': '<rootDir>/src/$1',
@@ -9,12 +17,4 @@ module.exports = {
     '^main$': '<rootDir>/__mocks__/main.ts',
   },
   testMatch: ['**/tests/**/*.test.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: {
-        module: 'commonjs',
-        esModuleInterop: true,
-      },
-    },
-  },
 };
